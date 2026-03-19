384e607435abd4f8981b6be66b31fffc53c801d5
itsAvii <avinashpradhan7777@gmail.com>
2026-03-19 20:44:30 +0530

    haa

--- database.js
+++ database.js
@@ -7,7 +7,39 @@
 // 4. Copy the configuration object
 
 
-s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  firebaseConfig = {
   apiKey: "AIzaSyDHfuCVvtRep_paqPw57W7h70JXT1kIkRI",
   authDomain: "unspoken-journey-29be5.firebaseapp.com",
